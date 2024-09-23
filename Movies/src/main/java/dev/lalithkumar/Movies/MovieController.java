0dd72743450562d5458b23a6505c48adf0d283bf
lalithkumar24 <lali22048.it@rmkec.ac.in>
2024-09-23 14:47:07 +0530

    connection and structure of api

---
+++ Movies/src/main/java/dev/lalithkumar/Movies/MovieController.java
@@ -0,0 +1,11 @@
+package dev.lalithkumar.Movies;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+
+}
---
+++ Movies/src/main/java/dev/lalithkumar/Movies/MoviesApplication.java
@@ -0,0 +1,25 @@
+package dev.lalithkumar.Movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+	@GetMapping("/")
+	public String API(){
+		return "API";
+	}
+	@GetMapping("/home")
+	public String APIHome(){
+		return "API Home";
+	}
+
+}
